41302f8a93f75e72f20008e1c248cc82fde45c89
David PV <davidpv@gmail.com>
2023-05-20 22:50:20 +0200

    feat: Add CNN pretrained model with VGG19 architecture
    This commit adds a new Jupyter notebook file containing code for a CNN pretrained model using the VGG19 architecture. The notebook includes code for loading and preprocessing images, splitting them into train and test datasets, checking class balance and number of channels, and creating the model with added layers. The summary of the model is also included in the notebook.

--- cnn_pretrained_model.py
+++ cnn_pretrained_model.py
@@ -1,25 +1,26 @@
-# !pip3 install split_folders
+# !pip install split_folders
+import os
+import splitfolders
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg19 import preprocess_input
 
-# import splitfolders
-# splitfolders.ratio('./datasets/dmd/labels', './datasets/dmd/labels_split', seed=1, ratio=(.9, 0, .1))
+# Preproceso de datos.
+
+if not os.path.exists('./datasets/dmd/labels_split'):
+    splitfolders.ratio('./datasets/dmd/labels', './datasets/dmd/labels_split', seed=1, ratio=(.9, 0, .1))
 
 INPUT_SHAPE = (224, 224, 3)
 DATA_DIR = './datasets/dmd/labels_split/data'
 BATCH_SIZE = 32
 IMG_SIZE = (180, 180)
-TEST_SPLIT = 0.2
-
-base_model = tf.keras.applications.VGG19(
-    weights='imagenet',
-    include_top=False
-)
+VAL_SPLIT = 0.2
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
     DATA_DIR,
     labels='inferred',
     label_mode='int',
-    validation_split=TEST_SPLIT,
+    validation_split=VAL_SPLIT,
     subset='training',
     color_mode='rgb',
     seed=1,
@@ -27,12 +28,15 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     image_size=IMG_SIZE,
     batch_size=BATCH_SIZE)
 
+preprocessed_train_ds = train_ds.map(
+    lambda x, y: (preprocess_input(x), y)
+)
 
 val_ds = tf.keras.utils.image_dataset_from_directory(
     DATA_DIR,
     labels='inferred',
     label_mode='int',
-    validation_split=TEST_SPLIT,
+    validation_split=VAL_SPLIT,
     subset='validation',
     color_mode='rgb',
     seed=1,
@@ -40,11 +44,15 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
     image_size=IMG_SIZE,
     batch_size=BATCH_SIZE)
 
-
 class_names = train_ds.class_names
 num_classes = len(class_names)
-print(class_names)
+print('Clases:' + class_names.join(','))
 
+
+base_model = tf.keras.applications.VGG19(
+    weights='imagenet',
+    include_top=False
+)
 
 x = base_model.output
 x = tf.keras.layers.GlobalAveragePooling2D()(x)
@@ -57,9 +65,9 @@ print(model.summary())
 print("Modelo base:", len(base_model.layers), "\nModelo:", len(model.layers))
 
 for layer in model.layers[:19]:
-    layer.trainable=False
+    layer.trainable = False
 for layer in model.layers[19:]:
-    layer.trainable=True
+    layer.trainable = True
 model.summary()
 
 model.compile(
