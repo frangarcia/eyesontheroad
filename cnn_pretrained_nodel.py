f01e06bbea1a009f3eff75bfda968f1a4d269e6d
David Perez <david.perez.vicens@lendrock.com>
2023-05-20 19:55:37 +0200

    Add gitignore and update model training code
    - Added datasets/dmd/labels_split/ to .gitignore
    - Updated cnn_model_training.ipynb with changes in the execution count, metadata, and outputs of some cells.
    - Created a new file cnn_pretrained_nodel.py for fine-tuning VGG19 on DMD dataset.

---
+++ cnn_pretrained_nodel.py
@@ -0,0 +1,79 @@
+# !pip3 install split_folders
+import tensorflow as tf
+
+# import splitfolders
+# splitfolders.ratio('./datasets/dmd/labels', './datasets/dmd/labels_split', seed=1, ratio=(.9, 0, .1))
+
+INPUT_SHAPE = (224, 224, 3)
+DATA_DIR = './datasets/dmd/labels_split/data'
+BATCH_SIZE = 32
+IMG_SIZE = (180, 180)
+TEST_SPLIT = 0.2
+
+base_model = tf.keras.applications.VGG19(
+    weights='imagenet',
+    include_top=False
+)
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    DATA_DIR,
+    labels='inferred',
+    label_mode='int',
+    validation_split=TEST_SPLIT,
+    subset='training',
+    color_mode='rgb',
+    seed=1,
+    shuffle=0,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE)
+
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    DATA_DIR,
+    labels='inferred',
+    label_mode='int',
+    validation_split=TEST_SPLIT,
+    subset='validation',
+    color_mode='rgb',
+    seed=1,
+    shuffle=0,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE)
+
+
+class_names = train_ds.class_names
+num_classes = len(class_names)
+print(class_names)
+
+
+x = base_model.output
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(512, activation='relu')(x)
+preds = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+
+model = tf.keras.models.Model(inputs=base_model.input, outputs=preds)
+print(model.summary())
+
+print("Modelo base:", len(base_model.layers), "\nModelo:", len(model.layers))
+
+for layer in model.layers[:19]:
+    layer.trainable=False
+for layer in model.layers[19:]:
+    layer.trainable=True
+model.summary()
+
+model.compile(
+    optimizer='Adam',
+    loss=tf.keras.losses.SparseCategoricalCrossentropy,
+    metrics=['accuracy']
+)
+
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+
+bestModel = model.fit(
+    train_ds,
+    validation_data=val_ds,
+    batch_size=BATCH_SIZE,
+    epochs=10,
+    callbacks=[early_stop]
+)
